7a7b97320d21fb61bed2e74ac325873e619ab364
KellyRoussel <kelly.roussel@hoomano.com>
2022-03-23 17:02:14 +0100

    change version number in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mojo-perception",
-    version="1.0.3",
+    version="1.0.4",
     author="Hoomano",
     author_email="contact@hoomano.com",
     description="Mojo Perception Python API",
